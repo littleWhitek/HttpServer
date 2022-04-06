170b57712264528350302f204b7ab1552eef9a43
littleWhitek <64830313+littleWhitek@users.noreply.github.com>
2022-04-06 22:17:09 +0800

    Add files via upload

---
+++ config.cpp
@@ -0,0 +1,41 @@
+#include "config.h"
+
+Config::Config(){
+    //端口号,默认9006 
+    PORT = 9006;
+
+    //数据库连接池数量,默认8
+    sql_num = 8;
+
+    //线程池内的线程数量,默认8
+    thread_num = 8;
+
+}
+
+void Config::parse_arg(int argc, char*argv[]){
+    int opt;
+    const char *str = "p:s:t:";
+    while ((opt = getopt(argc, argv, str)) != -1)
+    {
+        switch (opt)
+        {
+        case 'p':
+        {
+            PORT = atoi(optarg);
+            break;
+        }
+        case 's':
+        {
+            sql_num = atoi(optarg);
+            break;
+        }
+        case 't':
+        {
+            thread_num = atoi(optarg);
+            break;
+        }
+        default:
+            break;
+        }
+    }
+}
